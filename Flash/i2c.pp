ca161eb0bc1bf31d5a00ba69d6f3a0d8105658d2
FunkyFlier <mike@kaslingaircraft.com>
2016-01-26 08:35:47 -0600

    Initial Commit
    Project FOC ESC

---
+++ Flash/i2c.pp
@@ -0,0 +1,75 @@
+# FIXED
+
+i2c.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/i2c/src/32b/f28x/f2806x/i2c.c
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/DSP28x_Project.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/F2802x_Device.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/assert.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/linkage.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdbool.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/yvals.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/_lock.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stddef.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdint.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Adc.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_BootVars.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_DevEmu.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Comp.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_CpuTimers.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_ECap.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_EPwm.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Gpio.h
+i2c.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/i2c/src/32b/f28x/f2806x/I2c.h
+i2c.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/types/src/types.h
+i2c.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/string.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_NmiIntrupt.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieCtrl.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieVect.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Spi.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Sci.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_SysCtrl.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_XIntrupt.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_Examples.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_GlobalPrototypes.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_EPwm_defines.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_I2c_defines.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_DefaultISR.h
+i2c.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/cpu.h
+
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/i2c/src/32b/f28x/f2806x/i2c.c: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/DSP28x_Project.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/F2802x_Device.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/assert.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stddef.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdint.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Adc.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_BootVars.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_DevEmu.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Comp.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_CpuTimers.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_ECap.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_EPwm.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Gpio.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/i2c/src/32b/f28x/f2806x/I2c.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/types/src/types.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/string.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_NmiIntrupt.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieCtrl.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieVect.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Spi.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Sci.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_SysCtrl.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_XIntrupt.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_Examples.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_GlobalPrototypes.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_EPwm_defines.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_I2c_defines.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_DefaultISR.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/cpu.h: 
---
+++ Flash/proj_lab09.pp
@@ -0,0 +1,161 @@
+# FIXED
+
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/solutions/instaspin_foc/src/proj_lab09.c
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/math.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/linkage.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/solutions/instaspin_foc/src/main.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/math/src/32b/math.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/iqmath/src/32b/IQmathLib.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/limits.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/types/src/types.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdbool.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/yvals.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/_lock.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/string.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdint.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/memCopy/src/memCopy.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/32b/est.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_states.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/svgen/src/32b/svgen_current.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/fw/src/32b/fw.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/fem/src/32b/fem.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/math.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/cpu_usage/src/32b/cpu_usage.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/math.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/ctrl/src/32b/ctrl.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/clarke/src/32b/clarke.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/park/src/32b/park.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/ipark/src/32b/ipark.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/motor/src/32b/motor.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/offset/src/32b/offset.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/filter/src/32b/filter_fo.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/pid/src/32b/pid.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/svgen/src/32b/svgen.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/traj/src/32b/traj.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/ctrl/src/32b/ctrl_obj.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/ctrl/src/ctrl_states.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/hal/boards/boostxldrv8301_revB/f28x/f2802x/src/hal_obj.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/adc/src/32b/f28x/f2802x/adc.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/cpu/src/32b/f28x/f2802x/cpu.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/clk/src/32b/f28x/f2802x/clk.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pwm/src/32b/f28x/f2802x/pwm.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/flash/src/32b/f28x/f2802x/flash.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/gpio/src/32b/f28x/f2802x/gpio.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/osc/src/32b/f28x/f2802x/osc.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pie/src/32b/f28x/f2802x/pie.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pll/src/32b/f28x/f2802x/pll.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pwmdac/src/32b/f28x/f2802x/pwmdac.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pwr/src/32b/f28x/f2802x/pwr.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/spi/src/32b/f28x/f2802x/spi.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/timer/src/32b/f28x/f2802x/timer.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/wdog/src/32b/f28x/f2802x/wdog.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/drvic/drv8301/src/32b/f28x/f2802x/drv8301.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/cap/src/32b/f28x/f2802x/cap.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/drivers/i2c/src/32b/f28x/f2806x/i2c.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/usDelay/src/32b/usDelay.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/solutions/instaspin_foc/boards/boostxldrv8301_revB/f28x/f2802xF/src/user.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_Flux_states.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_Ls_states.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_Rs_states.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/fast/src/32b/userParams.h
+proj_lab09.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/hal/boards/boostxldrv8301_revB/f28x/f2802x/src/hal.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/F2802x_Device.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/assert.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h
+proj_lab09.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stddef.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Adc.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_BootVars.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_DevEmu.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Comp.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_CpuTimers.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_ECap.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_EPwm.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Gpio.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_NmiIntrupt.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieCtrl.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieVect.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Spi.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Sci.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_SysCtrl.h
+proj_lab09.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_XIntrupt.h
+
+C:/ti/motorware/motorware_1_01_00_14/sw/solutions/instaspin_foc/src/proj_lab09.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/math.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/linkage.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/solutions/instaspin_foc/src/main.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/math/src/32b/math.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/iqmath/src/32b/IQmathLib.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/limits.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/types/src/types.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/string.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdint.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/memCopy/src/memCopy.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/32b/est.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_states.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/svgen/src/32b/svgen_current.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/fw/src/32b/fw.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/fem/src/32b/fem.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/math.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/cpu_usage/src/32b/cpu_usage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/math.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/ctrl/src/32b/ctrl.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/clarke/src/32b/clarke.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/park/src/32b/park.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/ipark/src/32b/ipark.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/motor/src/32b/motor.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/offset/src/32b/offset.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/filter/src/32b/filter_fo.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/pid/src/32b/pid.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/svgen/src/32b/svgen.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/traj/src/32b/traj.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/ctrl/src/32b/ctrl_obj.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/ctrl/src/ctrl_states.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/hal/boards/boostxldrv8301_revB/f28x/f2802x/src/hal_obj.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/adc/src/32b/f28x/f2802x/adc.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/cpu/src/32b/f28x/f2802x/cpu.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/clk/src/32b/f28x/f2802x/clk.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pwm/src/32b/f28x/f2802x/pwm.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/flash/src/32b/f28x/f2802x/flash.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/gpio/src/32b/f28x/f2802x/gpio.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/osc/src/32b/f28x/f2802x/osc.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pie/src/32b/f28x/f2802x/pie.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pll/src/32b/f28x/f2802x/pll.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pwmdac/src/32b/f28x/f2802x/pwmdac.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/pwr/src/32b/f28x/f2802x/pwr.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/spi/src/32b/f28x/f2802x/spi.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/timer/src/32b/f28x/f2802x/timer.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/wdog/src/32b/f28x/f2802x/wdog.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/drvic/drv8301/src/32b/f28x/f2802x/drv8301.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/cap/src/32b/f28x/f2802x/cap.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/drivers/i2c/src/32b/f28x/f2806x/i2c.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/usDelay/src/32b/usDelay.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/solutions/instaspin_foc/boards/boostxldrv8301_revB/f28x/f2802xF/src/user.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_Flux_states.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_Ls_states.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/est/src/est_Rs_states.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/fast/src/32b/userParams.h: 
+C:/ti/motorware/motorware_1_01_00_14/sw/modules/hal/boards/boostxldrv8301_revB/f28x/f2802x/src/hal.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/F2802x_Device.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/assert.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.9/include/stddef.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Adc.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_BootVars.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_DevEmu.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Comp.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_CpuTimers.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_ECap.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_EPwm.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Gpio.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_NmiIntrupt.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieCtrl.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieVect.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Spi.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Sci.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_SysCtrl.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_XIntrupt.h: 
